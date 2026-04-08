29971c39404efd4138fed75f8b5abfcef7a6f5ac
sergi-villanueva <sergi.villanueva@inscastellbisbal.net>
2026-04-08 11:47:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // <--- ESTO ES CRUCIAL PARA ELECTRON
   server: {
     host: "::",
     port: 8080,
@@ -12,11 +13,17 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
+  build: {
+    outDir: "dist", // Asegura que la carpeta de salida se llame dist
+  }
 }));
