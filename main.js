f4b7a03229160612f6b69ea64b4426d1124ed7b2
sergi-villanueva <sergi.villanueva@inscastellbisbal.net>
2026-04-08 11:43:18 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Importante: Cargamos el archivo que genera Vite
+  win.loadFile(path.join(__dirname, 'dist/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
