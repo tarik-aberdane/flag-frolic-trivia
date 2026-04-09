72006bed414c6aff22d9d62ad7981899aa0256a8
lucafontanet-rgb <luca.fontanet@inscastellbisbal.net>
2026-04-09 10:42:40 +0200

    Add service worker for offline support and caching strategy

---
+++ public/service-worker.js
@@ -0,0 +1,46 @@
+// service-worker.js
+
+const CACHE_NAME = 'flag-frolic-trivia-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return the cached response if found, otherwise fetch from the network
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
