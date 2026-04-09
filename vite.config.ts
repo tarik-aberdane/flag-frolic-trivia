dadc54e048b3a26cdbc2f16716793b35c52afe84
tarik-aberdane <tarik.aberdane@inscastellbisbal.net>
2026-04-09 16:07:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // base: './' asegura que Electron encuentre los archivos JS/CSS en la carpeta local
-  base: './',
-  plugins: [
-    react(),
-  ],
+  // ESTA ES LA LINEA MÁS IMPORTANTE PARA ELECTRON
+  base: './', 
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
 });
